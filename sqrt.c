d01a3f7a434fd6a022cff4f3ef6fd83c7b267809
pushkar79k1-commits <pushkar79k1@gmail.com>
2026-03-17 14:09:44 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("enter anumber : ");
+    scanf("%d",&a);
+    int root =sqrt(a);
+    printf("the sqare root of no is %d",root);
+    return 0;
+}
