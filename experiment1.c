d67eefdef7704719cfd6c353006c058ce3d56b38
pushkar79k1-commits <pushkar79k1@gmail.com>
2026-03-17 14:01:27 +0530

    Add files via upload

---
+++ experiment1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e;
+    printf("enter a marks a : ");
+    scanf("%d",&a);
+    printf("enter a marks b :");
+    scanf("%d",&b);
+    printf("enter a marks c: ");
+    scanf("%d",&c);
+    printf("enter a mark d: ");
+    scanf("%d",&d);
+    printf("enter a mark e: ");
+    scanf("%d",&e);
+    int s;
+    s = a+b+c+d+e;
+    printf("sum of the number %d",s);
+}
