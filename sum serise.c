56b25daa99c791d7c47868dd3734c89b7d75d1e7
pushkar79k1-commits <pushkar79k1@gmail.com>
2026-03-17 14:05:11 +0530

    Add files via upload

---
+++ sum serise.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a no : ");
+    scanf("%d",&n);
+    int s=0;
+    for(int i=0;i<0;i++){
+       if(i%2!=0)
+       s=s+i;
+       printf("sum of odd no series %d",s);
+    }
+
+}
